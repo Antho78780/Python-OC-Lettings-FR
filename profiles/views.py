61d06a33cb1bfeaa40c31bc8946d9aa325ef5b56
Anthony Audrain <anthonyaud78@gmail.com>
2023-10-16 16:53:59 +0200

    modification du profiles_index_html en index.html

--- profiles/views.py
+++ profiles/views.py
@@ -4,4 +4,4 @@ from profiles.models import Profile
 def index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
