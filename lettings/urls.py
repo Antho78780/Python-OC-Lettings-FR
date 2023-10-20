3910047212c08dcdd3d27371e6f1503bb2a9dc35
Anthony Audrain <anthonyaud78@gmail.com>
2023-10-20 14:45:44 +0200

    ajout des tests

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lettings/', views.index, name='lettings_index'),
+    path('', views.index, name='lettings_index'),
+    path('<int:letting_id>/', views.letting, name='letting'),
 ]
