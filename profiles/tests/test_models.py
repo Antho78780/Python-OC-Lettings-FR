3bf18d0dca4beb63a39ee6c06b89fd397e81e9c4
anthony <anthonyaud78@gmail.com>
2023-10-25 01:49:55 +0200

    ajout de la méthode setUp

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from profiles.models import Profile
 from django.contrib.auth.models import User
-import pytest
 
 class TestModel(TestCase):
-    @pytest.mark.django_db
-    def test_modelProfiles(self):
-        pass
+    def setUp(self):
+        self.user = User.objects.create(
+            username="Antho"
+        )
+        self.profile = Profile.objects.create(
+            user=self.user, 
+            favorite_city="maurecourt"
+        )
+
+    def test_profile_model(self):
+        expected_value = "Antho"
+        self.assertEqual(str(self.profile), expected_value)
