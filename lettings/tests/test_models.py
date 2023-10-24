f7090748dc7703ca85a9ea1d42a9a957a031a8ca
anthony <anthonyaud78@gmail.com>
2023-10-25 01:40:52 +0200

    ajout de la méthode setUp

--- lettings/tests/test_models.py
+++ lettings/tests/test_models.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 from lettings.models import Letting, Addres
-import pytest
 
-
-@pytest.mark.django_db
 class TestModel(TestCase):
-    def test_modelAdress(self):
-        address = Addres.objects.create(number=5, street="l'oise", city="paris", state="france", zip_code=78567, country_iso_code="FRA")
-        expected_value = "5 l'oise"
-        assert str(address) == expected_value
+    def setUp(self):
+        self.address = Addres.objects.create(
+            number=5,
+            street="l'oise", 
+            city="paris", 
+            state="france", 
+            zip_code=78567, 
+            country_iso_code="FRA"
+        )
+        self.letting = Letting.objects.create(
+            title="location1", 
+            address=self.address
+        )
     
-    def test_modelLetting(self):
-        address = Addres.objects.create(number=5, street="l'oise", city="paris", state="france", zip_code=78567, country_iso_code="FRA")
-        letting = Letting.objects.create(title="location1", address=address)
+    def test_address_model(self):
+        expected_value = "5 l'oise"
+        self.assertEqual(str(self.address), expected_value)
+    
+    def test_letting_model(self):
         expected_value = "location1"
-        assert str(letting) == expected_value
+        self.assertEqual(str(self.letting), expected_value)
+
