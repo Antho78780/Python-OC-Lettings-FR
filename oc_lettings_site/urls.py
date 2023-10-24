6d9ed2d104cbb6d361d3e954a599da7f3d02d0f6
anthony <anthonyaud78@gmail.com>
2023-10-25 01:45:39 +0200

    ajout de handler 404 et 500

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,16 +5,18 @@ from . import views
 
 
 
-# def trigger_error(request):
-#     division_by_zero = 1 / 0
-#     return division_by_zero
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
 
 
 urlpatterns = [
-    # path('sentry-debug/', trigger_error),
+    path('sentry-debug/', trigger_error),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
+handler404 = 'oc_lettings_site.views.error_404_view'
+handler500 = 'oc_lettings_site.views.error_500_view'
 
