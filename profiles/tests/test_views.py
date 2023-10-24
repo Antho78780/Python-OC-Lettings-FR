9360e62f84e63d8beb4d828c88002816611f6337
anthony <anthonyaud78@gmail.com>
2023-10-25 01:51:08 +0200

    ajout de la vérification des templates

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -1,19 +1,24 @@
-from pytest_django.asserts import assertTemplateUsed
-import pytest
 from profiles.models import Profile
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
+from django.urls import reverse
 
-@pytest.mark.django_db
+
 class TestView(TestCase):
     client = Client()
 
     def test_indexProfile(self):
         response = self.client.get('/profiles/')
-        assertTemplateUsed(response, "profiles/index.html")
-        assert response.status_code == 200
+        self.assertTemplateUsed(response, "profiles/index.html")
+        self.assertEqual(response.status_code, 200)
     
     def test_profiles(self):
-        pass
+        user = User.objects.create(username='Antho')
+        profile = Profile.objects.create(user=user, favorite_city='maurecourt')
+        path = reverse('profile', kwargs={'username': 'Antho'})
+        response = self.client.get(path)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+        self.assertEqual(response.status_code, 200)
 
 
 
