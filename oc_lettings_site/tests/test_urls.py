9bac80450b0a9fff3390abf29337d6fff996ec38
anthony <anthonyaud78@gmail.com>
2023-10-25 15:21:13 +0200

    ajout des tests

---
+++ oc_lettings_site/tests/test_urls.py
@@ -0,0 +1,4 @@
+from django.test import TestCase, Client
+
+class TestUrls(TestCase):
+    pass
---
+++ oc_lettings_site/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, Client
+
+class TestViews(TestCase):
+    cient = Client()
+
+    # def test_dummy():
+    #     assert 1
+
+    def test_view_index(self):
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_error_404_view(self):
+        pass
