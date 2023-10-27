16e7754fd204677db2e38815e4a6697117c7047d
Anthony Audrain <anthonyaud78@gmail.com>
2023-10-27 21:52:02 +0200

    ajout de docstrings pour la fonction index et letting

--- lettings/views.py
+++ lettings/views.py
@@ -3,12 +3,14 @@ from lettings.models import Letting
 
 
 def index(request):
+    """Return the request, the gabarit and the context which contains the list of profiles."""
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings/index.html', context)
 
 
 def letting(request, letting_id):
+    """Return the request, the gabarit and the context which contains the profile."""
     letting = Letting.objects.get(id=letting_id)
     context = {
         'title': letting.title,
