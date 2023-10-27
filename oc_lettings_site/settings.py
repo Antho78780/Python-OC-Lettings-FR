d698b37584cd2f1da475fdb1bb5e88c5fb706cfc
Anthony Audrain <anthonyaud78@gmail.com>
2023-10-27 22:01:08 +0200

    importation de dotenv + rajout du parametre DEFAULT_AUTO_FIELD ligne 128

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,8 @@
 import os
 import sentry_sdk
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -102,8 +104,6 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 
 sentry_sdk.init(
-    dsn="https://d7b082990a6645e05ddb1606d8eb28f6@o4506060795740160.ingest.sentry.io/4506060797509632",
+    dsn=os.getenv('SENTRY_DSN'),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
@@ -125,3 +125,4 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
