027a7860bb30fd7bd82d21b54133f39795545f83
Anthony Audrain <anthonyaud78@gmail.com>
2023-12-11 13:39:14 +0100

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -111,7 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
@@ -119,7 +120,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
